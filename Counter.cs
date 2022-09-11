68a65e97869c7140feb56a5ff8948ca35ce7bcb2
Sunny995 <51695320+Sunny995@users.noreply.github.com>
2022-09-11 22:51:44 +0530

    Add files via upload

---
+++ Counter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Counter
+    {
+        private static readonly Counter instance = new Counter();
+        
+        public static Counter Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public int count;
+
+        public  void Increament() 
+        {
+            this.count++;
+        }
+
+        public void Decreament()
+        {
+            this.count--;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter.Instance.Increament();
+            Counter.Instance.Decreament();
+            Console.WriteLine(Counter.Instance.count);
+        }
+    }
+}
